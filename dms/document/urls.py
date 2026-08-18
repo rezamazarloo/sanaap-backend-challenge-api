3bade7ffac427f693550d5aaabd1a8c25f96739e
reza <rezamazarloo@yahoo.com>
2026-08-19 02:44:11 +0330

    added urls

--- dms/document/urls.py
+++ dms/document/urls.py
@@ -1,13 +1,34 @@
 from django.urls import path
-from document.views import DocumentDownloadDeleteView, DocumentListCreateView
+from document.views import (
+    AllDocumentListView,
+    DocumentDetailView,
+    DocumentListCreateView,
+    DocumentTypeDetailView,
+    DocumentTypeListCreateView,
+    UserDocumentCreateView,
+)
 
 app_name = "document"
 
 urlpatterns = [
+    path(
+        "types/", DocumentTypeListCreateView.as_view(), name="document-type-list-create"
+    ),
+    path(
+        "types/<int:document_type_id>/",
+        DocumentTypeDetailView.as_view(),
+        name="document-type-detail",
+    ),
+    path("all/", AllDocumentListView.as_view(), name="document-all-list"),
+    path(
+        "users/<int:user_id>/",
+        UserDocumentCreateView.as_view(),
+        name="user-document-create",
+    ),
     path("", DocumentListCreateView.as_view(), name="document-list-create"),
     path(
         "<int:document_id>/",
-        DocumentDownloadDeleteView.as_view(),
-        name="document-download-delete",
+        DocumentDetailView.as_view(),
+        name="document-detail",
     ),
 ]
