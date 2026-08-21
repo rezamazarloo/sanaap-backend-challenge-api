62cbd2d2a853c40942c1606faf82e9bd093378f2
reza <rezamazarloo@yahoo.com>
2026-08-22 00:01:04 +0330

    added asgi config

--- dms/config/asgi.py
+++ dms/config/asgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from config.middleware import TokenAuthMiddleware
+from notification.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": TokenAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    }
+)
